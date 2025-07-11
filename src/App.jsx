8dae8dde94f271593fa71e5f33779ce6d797902e
federicapaglia96 <federicapaglia96@gmail.com>
2025-07-11 18:20:17 +0200

    feat: footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import HomePage from './Pages/Homepage.jsx'
@@ -11,16 +11,17 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
-    <GuestLayout />
-    <Routes>
-      <Route path='/' element={<HomePage />}  />
-      <Route path='/productDetails/:id' element={<ProductPage />} />
-    </Routes>
-    
-    
-    
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<GuestLayout />}>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/productDetails/:id' element={<ProductPage />} />
+          </Route>
+        </Routes>
+
+
+
+      </BrowserRouter>
 
     </>
   )
