501762ce7817bf4df35808fe29e2797277a7e836
FernandoSalicandro <studio.salicandro@gmail.com>
2025-07-11 12:59:46 +0200

    feat: import css, header, hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './Pages/Homepage.jsx'
 import ProductPage from './Pages/ProductPage.jsx'
+import GuestLayout from './layout/GuestLayout.jsx'
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
     <BrowserRouter>
+    <GuestLayout />
     <Routes>
       <Route path='/' element={<HomePage />}  />
       <Route path='/productDetails' element={<ProductPage />} />
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,6 +1,15 @@
 export default function HomePage() {
     return (
         <>
+        <main>
+            <div className="hero-section">
+                <img src="img\heroFullscreen.png" alt="" />
+            </div>
+            
+            <h2 className="text-center my-5">JW FOR ...</h2>
+
+        </main>
+            
         </>
     )
 }
