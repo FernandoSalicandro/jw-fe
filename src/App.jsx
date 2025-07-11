00db2e069cd7f415823f96693caf23290d00574b
FernandoSalicandro <studio.salicandro@gmail.com>
2025-07-11 10:53:33 +0200

    feat:aggiune rotte di base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
 
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />}  />
+      <Route path='/productDetails' element={<ProductPage />} />
+    </Routes>
+    
+    
+    
+    </BrowserRouter>
 
     </>
   )
