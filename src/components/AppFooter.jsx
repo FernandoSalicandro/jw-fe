5aa3e48347f260e8973a9f9c8443cce6abdf326d
federicapaglia96 <federicapaglia96@gmail.com>
2025-07-12 10:19:00 +0200

    feat: footer aggiornato

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,41 +2,46 @@
 const AppFooter = () => {
     return (
         <footer className="footer">
-            <div className="column">
-                <h4>SHOP</h4>
-                <ul>
-                    <li><a href="#">Anelli</a></li>
-                    <li><a href="#">Bracciali</a></li>
-                    <li><a href="#">Collane</a></li>
-                    <li><a href="#">Orecchini</a></li>
-                </ul>
-            </div>
-
-            <div className="column">
-                <h4>CLIENT SERVICES</h4>
-                <ul>
-                    <li><a href="#">Chi siamo</a></li>
-                    <li><a href="#">Servizi clienti</a></li>
-                    <li><a href="#">Spedizione</a></li>
-                    <li><a href="#">Resi e rimborsi</a></li>
-                </ul>
-            </div>
-
-            <div className="column">
-                <h4>CONTACT</h4>
-                <p>Email:</p>
-                <p>Tel:</p>
-                <div className="social-icons">
+            <div className="container-footer">
+                <div className="footer-column">
+                    <h4>SHOP</h4>
                     <ul>
-                        <li><a href="#">twitter</a></li>
-                        <li><a href="#">facebook</a></li>
-                        <li><a href="#">instagram</a></li>
+                        <li><a href="#">Anelli</a></li>
+                        <li><a href="#">Bracciali</a></li>
+                        <li><a href="#">Collane</a></li>
+                        <li><a href="#">Orecchini</a></li>
                     </ul>
                 </div>
-            </div>
-            <div className="column">
-                <h4>ISCRIZIONE</h4>
-                <p>Scopri in anteprima i nuovi arrivi dal nostro straordinario laboratorio, gli eventi speciali e altre novità dal mondo di JWLUX.</p>
+
+                <div className="footer-column">
+                    <h4>CLIENT SERVICES</h4>
+                    <ul>
+                        <li><a href="#">Chi siamo</a></li>
+                        <li><a href="#">Servizi clienti</a></li>
+                        <li><a href="#">Spedizione</a></li>
+                        <li><a href="#">Resi e rimborsi</a></li>
+                    </ul>
+                </div>
+
+                <div className="footer-column">
+                    <h4>CONTACT</h4>
+                       <ul>
+                        <li><a href="#">Email</a></li>
+                        <li><a href="#">Telefono</a></li>
+                        <li><a href="#">Indirizzo</a></li>
+                    </ul>
+                    <div className="social-icons">
+                        <ul>
+                            <li><a href="#">twitter</a></li>
+                            <li><a href="#">facebook</a></li>
+                            <li><a href="#">instagram</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer-column">
+                    <h4>ISCRIZIONE</h4>
+                    <p>Scopri in anteprima i nuovi arrivi dal nostro straordinario laboratorio, gli eventi speciali e altre novità dal mondo di JWLUX.</p>
+                </div>
             </div>
         </footer>
     )
