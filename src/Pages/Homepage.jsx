d5d69080f58215ee625b7de17e6c3f973d29a268
FernandoSalicandro <studio.salicandro@gmail.com>
2025-07-12 10:11:06 +0200

    feat: aggiunto scroll behavior al carosello homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,8 +1,21 @@
-import {useContext} from 'react';
+import {useContext, useRef} from 'react';
 import {ProductContext} from '../Context/ProductContext.jsx';
 
 export default function HomePage() {
     const {products} = useContext(ProductContext);
+    const carouselRef = useRef(null);
+
+    const handleScroll = (direction) => {
+        if(carouselRef.current) {
+            const scrollAmount = 300;
+            const scrollLeft = carouselRef.current.scrollLeft;
+            carouselRef.current.scrollTo({
+                left : direction === 'left' ? scrollLeft -scrollAmount : scrollLeft + scrollAmount,
+                behavior : 'smooth'
+            
+            })
+        }
+    }
     return (
         <>
             <main>
@@ -12,44 +25,20 @@ export default function HomePage() {
 
                 <h2 className="text-center my-5">JW FOR ...</h2>
                 <div className="container">
-                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2 carousel-container">
+                    <div
+                    ref={carouselRef} 
+                    className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2 carousel-container">
                         {products.map(curProduct => (
                              <div className="col" key={curProduct.id}>
                             <div className="card">
-                                <div className="card-header">{curProduct.name}</div>
+                                <p className='text-center mt-1 mb-0 text-em'>{curProduct.name}</p>
                                 <div className="card-body"><img src={curProduct.image_url} alt="" /></div>
                             </div>
                         </div>
                         ))}
-                       
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-header">TITOLO DELLA CARD</div>
-                                <div className="card-body">TITOLO DELLA CARD</div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-header">TITOLO DELLA CARD</div>
-                                <div className="card-body">TITOLO DELLA CARD</div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-header">TITOLO DELLA CARD</div>
-                                <div className="card-body">TITOLO DELLA CARD</div>
-                            </div>
-                        </div>
-                     
-                      
-                     
                     </div>
                 </div>
                
-                
-
-
-
             </main>
 
         </>
