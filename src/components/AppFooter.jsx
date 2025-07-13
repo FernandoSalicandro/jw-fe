48e3df00e283bd0a1c1a39abc3138c0b7d828378
federicapaglia96 <federicapaglia96@gmail.com>
2025-07-13 12:11:20 +0200

    feat: modifiche footer

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -4,6 +4,16 @@ const AppFooter = () => {
         <footer className="footer">
             <div className="container-footer">
                 <div className="footer-column">
+                    <h4>JW LUX</h4>
+                    <p>Please call or email for any information or to make an appointment at one of our boutiques.</p>
+                    <p>+1 (212) 421 3030</p>
+                    <div className="footer-social">
+                        <a href="#"><i className="fab fa-facebook-f"></i></a>
+                        <a href="#"><i className="fab fa-instagram"></i></a>
+                        <a href="#"><i className="fab fa-twitter"></i></a>
+                        </div>
+                </div>
+                <div className="footer-column">
                     <h4>SHOP</h4>
                     <ul>
                         <li><a href="#">Anelli</a></li>
@@ -25,22 +35,15 @@ const AppFooter = () => {
 
                 <div className="footer-column">
                     <h4>CONTACT</h4>
-                       <ul>
-                        <li><a href="#">Email</a></li>
-                        <li><a href="#">Telefono</a></li>
-                        <li><a href="#">Indirizzo</a></li>
+                    <ul>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Book an Appointment</a></li>
+                        <li><a href="#">Carrers</a></li>
                     </ul>
-                    <div className="social-icons">
-                        <ul>
-                            <li><a href="#">twitter</a></li>
-                            <li><a href="#">facebook</a></li>
-                            <li><a href="#">instagram</a></li>
-                        </ul>
-                    </div>
                 </div>
                 <div className="footer-column">
-                    <h4>ISCRIZIONE</h4>
-                    <p>Scopri in anteprima i nuovi arrivi dal nostro straordinario laboratorio, gli eventi speciali e altre novità dal mondo di JWLUX.</p>
+                    <h4>Sign Up</h4>
+                    <p>Scopri in anteprima i nuovi arrivi dal nostro straordinario laboratorio, gli eventi speciali e altre novità dal mondo di JW LUX.</p>
                 </div>
             </div>
         </footer>
