404e0195b1752eb03cff27b0d12a3ee21ce3a742
federicapaglia96 <federicapaglia96@gmail.com>
2025-07-14 16:29:39 +0200

    ringsPages modifiche

--- src/Pages/RingPage.jsx
+++ src/Pages/RingPage.jsx
@@ -6,7 +6,20 @@ export default function RingPage() {
                               <img src="./public/img/emerald-dome-ring.png" />
                               <h4 className="heroRing-text">Rings</h4>
                         </div>
-              
+            <div className="ring-cards-container">
+                  <div className="ring-card">
+                        <img src="" alt="Ring 1" className="ring-card-img" />
+                        <p className="ring-card-title">Emerald Ring</p>
+                  </div>
+                  <div className="ring-card">
+                        <img src="" alt="Ring 2" className="ring-card-img" />
+                        <p className="ring-card-title">Diamond Ring</p>
+                  </div>
+                  <div className="ring-card">
+                        <img src="" alt="Ring 3" className="ring-card-img" />
+                        <p className="ring-card-title">Sapphire Ring</p>
+                  </div>
+            </div>
 
             </>
       )
