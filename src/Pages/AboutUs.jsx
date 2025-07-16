d5cc72e37e5612100c143a8ff164dc0f108792db
federicapaglia96 <federicapaglia96@gmail.com>
2025-07-16 21:45:41 +0200

    inizio pages aboutus

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,28 +1,47 @@
 export default function AboutUs() {
     return (
         <>
-        <div className="main-page">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est doloremque voluptatem ipsum nisi deleniti cumque libero, veniam consequuntur similique quod dolore! Facere assumenda repellendus optio harum necessitatibus molestias. Hic, impedit.
-            Facere dolorum iste ex, ipsam, animi in earum odio eveniet magnam repellendus porro ab sequi nostrum ipsa quasi cupiditate quaerat consequatur soluta autem atque est corporis. Libero, exercitationem quis? Consequatur!
-            Iure, ducimus sapiente! Distinctio maiores placeat obcaecati delectus sapiente architecto quidem aliquid molestiae voluptate qui? Culpa itaque labore sed totam? Reprehenderit facilis animi officia incidunt assumenda cumque provident non laudantium!
-            Ipsum qui omnis eos vitae, quo eius tempora ex reprehenderit quibusdam illo aliquid sunt ratione magnam at saepe molestias, assumenda numquam, esse inventore quas. Voluptatibus esse tempora alias libero nihil?
-            At laboriosam reprehenderit tenetur porro velit fugiat adipisci pariatur voluptates repellendus facilis, voluptas, non impedit quas nesciunt sint? Consequatur, perspiciatis! Necessitatibus, possimus exercitationem. Amet quam laboriosam asperiores necessitatibus molestias id!
-            Perferendis facere tempore eligendi quidem quibusdam. Quo, ex rem itaque tenetur quidem voluptatem porro ut beatae rerum perspiciatis fuga facere? Optio, dolorem illum voluptates voluptate quam tempora quidem nulla modi?
-            Quo similique quibusdam accusamus in eveniet, qui excepturi. Nemo dolorum voluptatem molestiae hic quo officiis! Nesciunt dolor quae animi provident, consequatur repellendus aliquam culpa tempora, nihil odio sint optio blanditiis?
-            Exercitationem aut magni debitis iure laudantium officia nemo ut illum nihil nesciunt cum reprehenderit distinctio enim voluptatum quaerat dolores assumenda quasi, dolorum iusto animi ullam eligendi temporibus. Maxime, iure quam?
-            Consectetur, dolore? Corrupti nam incidunt quis ex, iusto repellat molestiae sed! Sapiente voluptatem ratione odio deserunt laboriosam, perferendis nulla labore nobis quos dolore animi adipisci delectus voluptates reiciendis quo. Quod?
-            Dolore iusto animi minima neque ipsam autem quaerat voluptatum explicabo fuga ea. Esse illo odio a iure sunt laborum aliquam atque veniam, sint, consequatur maxime quisquam reiciendis, enim amet dolores.
-            Cupiditate libero fugiat esse ipsum officia impedit, in iure molestias officiis voluptates non incidunt adipisci repellat ut pariatur, earum ea, distinctio recusandae dolorum reiciendis mollitia. Quia reprehenderit nostrum aliquid facere!
-            Ea eos eligendi cupiditate dolores veritatis sunt consequuntur adipisci, quia dicta veniam, voluptatibus deleniti corrupti rerum delectus blanditiis architecto, maiores ipsum repellat magni sint quasi error! Hic architecto nesciunt enim!
-            Rem sunt sapiente praesentium ab tenetur labore eius ea ex esse unde repudiandae corporis hic suscipit, voluptatum vel quaerat culpa incidunt quam ad? Quibusdam reprehenderit iure dolores omnis aperiam quod.
-            Magnam et minima aspernatur ratione, vero eos maxime velit fuga enim pariatur reprehenderit quas non, quia, repudiandae eligendi consectetur exercitationem. Reiciendis ea fugit consequuntur non. Quibusdam impedit quia nihil cum.
-            Natus sit maiores quia rerum ullam fuga hic, minus at quas quis ut est esse quaerat assumenda ipsum molestias iusto, inventore quibusdam suscipit dicta. Deserunt soluta quas recusandae laudantium quisquam.
-            Itaque dolorem ab omnis ducimus aliquam delectus quibusdam ratione, accusamus quo necessitatibus, aut fugiat tenetur ipsa voluptatum magni ad non neque quae dolorum repudiandae consequuntur, sequi nam! Dolorum, iure neque!
-            Repudiandae adipisci natus aliquam optio incidunt accusantium nesciunt recusandae nobis, voluptatibus reprehenderit repellendus doloremque quas odio aliquid harum tempora labore asperiores sapiente sunt, at vero porro beatae odit! Nihil, sed.
-            Quia, adipisci quo porro distinctio consectetur aliquid rem modi delectus dignissimos, quasi non cum inventore numquam reprehenderit doloribus qui deserunt possimus unde eos aspernatur est. Quae, dolor. Voluptas, saepe! Dolores?
-            Consequuntur libero consectetur sit, numquam cumque eaque, ratione optio, ut exercitationem dolorem quidem et aperiam facilis dignissimos suscipit corporis non nostrum culpa eveniet similique. Magnam beatae aliquam quaerat id obcaecati?
-            Ullam aut ab tempore, sit sint et sequi architecto rem corrupti quis sed aliquid placeat nobis. Eius, ipsa blanditiis? Repudiandae excepturi quis eveniet doloribus magni eaque rerum hic eligendi id.</p>
+        <div className="page-main">
+    {/* prima immagine */}
+    <div className="aboutus-card-row" style={{ display: 'flex', alignItems: 'center', marginBottom: '1rem' }}>
+        <div className="aboutus-card-text" style={{ flex: 1, padding: '1rem' }}>
+            <h2>La nostra storia</h2>
+            <p>
+                Siamo un team appassionato di gioielli, dediti a creare pezzi unici che raccontano una storia. Ogni collezione nasce da un'ispirazione autentica e da una ricerca continua della qualità.
+            </p>
+        </div>
+        <div className="aboutus-card-img" style={{ flex: 1, textAlign: 'right' }}>
+            <img src="/img/brand-story-img.png" alt="La nostra storia" style={{ maxWidth: '80%', width: '100%', margin: '9px' }} />
+        </div>
+    </div>
+
+    {/* seconda immagine */}
+                <div className="aboutus-card-row" style={{ display: 'flex', alignItems: 'center', flexDirection: 'row-reverse' }}>
+                    <div className="aboutus-card-text" style={{ flex: 1, padding: '1rem' }}>
+                        <h2>I nostri valori</h2>
+                        <p>
+                            Crediamo nell'artigianalità, nella sostenibilità e nell'attenzione ai dettagli. Ogni gioiello è realizzato con cura per accompagnarti nei momenti più importanti della tua vita.
+                        </p>
+                    </div>
+                    <div className="aboutus-card-img" style={{ flex: 1, textAlign: 'left' }}>
+                        <img src="/img/hero-homepage.png" alt="I nostri valori" style={{ maxWidth: '80%', margin:'9px' }} />
+                    </div>
+                </div>
         </div>
         </>
     )
 }
+
+          <div className="container">
+                    <div className="row row-cols-2 d-flex text-cente brand-story-img">
+                        <div className="col p-0"><img src="img/brand-story-img.png" alt="" /></div>
+                        <div className="col p-0 text-center">Ciao</div>
+                    </div>
+                    <div className="row row-cols-2 d-flex text-cente brand-story-img">
+                        <div className="col p-0">Ciao</div>
+                        <div className="col p-0"><img src="img/brand-story-img.png" alt="" /></div>
+
+                    </div>
+
+
+                </div>
