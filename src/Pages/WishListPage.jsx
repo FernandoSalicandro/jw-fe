79664a58f163fb0118e8aafac37fa2060a50706e
PintilieGeani <pintilie.geany@yahoo.it>
2025-07-18 02:04:37 +0200

    feat: badge-personalizzato

--- src/Pages/WishListPage.jsx
+++ src/Pages/WishListPage.jsx
@@ -47,56 +47,64 @@ export default function WishListPage() {
         <div className="container section-separator p-2 mb-3">
           <h1>Your Desires...</h1>
         </div>
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4">
-          {wishList.length > 0 &&
-            wishList.map((curWish) => (
-              <>
-                <div className="col-md-4 mb-4 image-price" key={curWish.id}>
-                  <div className="card h-100 border-0 mb-5">
-                    <img onClick={() => navigate(`/productDetails/${curWish.slug}`)} src={curWish.image_url} className="card-img-top hover-img mb-2" alt={curWish.name} />
-                    <div className="card-body text-center">
-                      <h5 className="card-title">{curWish.name}</h5>
-                      {curWish.is_promo === 1 ? (
-                        <p className="card-text prezzo">
-                          <span>{curWish.discount_price} €</span>
-                        </p>
-                      ) : (
-                        <p className="card-text prezzo">{curWish.price} €</p>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </>
-            ))}
-        </div>
-
-        {relatedProducts.length > 0 && (
-          <>
-            <h2 className="text-center mb-5">May You Like Also...</h2>
-            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
-              {relatedProducts.map((product) => {
-                console.log(product.name, product.is_promo);
-                return (
-                  <div className="col mb-4 image-price" key={product.id}>
+        <div className="container">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4">
+            {wishList.length > 0 &&
+              wishList.map((curWish) => (
+                <>
+                  <div className="col-md-4 mb-4 image-price" key={curWish.id}>
                     <div className="card h-100 border-0 mb-5">
-                      <img onClick={() => navigate(`/productDetails/${product.slug}`)} src={product.image_url} className="card-img-top hover-img mb-2" alt={product.name} />
+                      <div className={curWish.is_promo === 1 ? "image-price card-custom overflow border-0 rounded" : "image-price overflow border-0 rounded"}>
+                        <img onClick={() => navigate(`/productDetails/${curWish.slug}`)} src={curWish.image_url} alt={curWish.name} className="card-img-top  hover-img" />
+                        {curWish.is_promo === 1 && <img className="discount-logo" src="img/jw_logo_discount.png" alt="logo-discount" />}
+                      </div>
                       <div className="card-body text-center">
-                        <h5 className="card-title">{product.name}</h5>
-                        {product.is_promo === 1 ? (
+                        <h5 className="card-title">{curWish.name}</h5>
+                        {curWish.is_promo === 1 ? (
                           <p className="card-text prezzo">
-                            <span>{product.discount_price} €</span>
+                            <span>{curWish.discount_price} €</span>
                           </p>
                         ) : (
-                          <p className="card-text prezzo">{product.price} €</p>
+                          <p className="card-text prezzo">{curWish.price} €</p>
                         )}
                       </div>
                     </div>
                   </div>
-                );
-              })}
-            </div>
-          </>
-        )}
+                </>
+              ))}
+          </div>
+
+          {relatedProducts.length > 0 && (
+            <>
+              <h2 className="text-center mb-5">May You Like Also...</h2>
+              <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+                {relatedProducts.map((product) => {
+                  console.log(product.name, product.is_promo);
+                  return (
+                    <div className="col mb-4 image-price" key={product.id}>
+                      <div className="card h-100 border-0 mb-5">
+                        <div className={product.is_promo === 1 ? "image-price card-custom overflow border-0 rounded" : "image-price overflow border-0 rounded"}>
+                          <img onClick={() => navigate(`/productDetails/${product.slug}`)} src={product.image_url} alt={product.name} className="card-img-top  hover-img" />
+                          {product.is_promo === 1 && <img className="discount-logo" src="img/jw_logo_discount.png" alt="logo-discount" />}
+                        </div>
+                        <div className="card-body text-center">
+                          <h5 className="card-title">{product.name}</h5>
+                          {product.is_promo === 1 ? (
+                            <p className="card-text prezzo">
+                              <span>{product.discount_price} €</span>
+                            </p>
+                          ) : (
+                            <p className="card-text prezzo">{product.price} €</p>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </>
+          )}
+        </div>
       </main>
     </>
   );
